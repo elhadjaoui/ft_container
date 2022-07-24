112892b68b23e77cc5fa4a228e885bc1ad5eed5b
elhadjaoui <acharf-2060@outlook.fr>
2022-07-24 16:33:17 +0100

    woriking on Vector

--- header/vector.hpp
+++ header/vector.hpp
@@ -18,18 +18,30 @@
 #include <iterator>
 #include <map>
 #include <vector>
+#include <memory>
 
 
 namespace ft
 {
-    template < class T, class Alloc = allocator<T> > 
+    template < class T, class Alloc = std::allocator<T>> 
     class vector
     {
     private:
         
     public:
-        vector();
-        ~vector();
+        public:
+		typedef T 											value_type;
+		typedef Alloc										allocator_type;
+		typedef typename allocator_type::reference			reference;
+		typedef typename allocator_type::const_reference 	const_reference;
+		typedef typename allocator_type::pointer			pointer;
+		typedef typename allocator_type::const_pointer		const_pointer;
+        typedef ptrdiff_t                                   difference_type;
+        typedef size_t										size_type;
+		typedef myIter<pointer>								iterator;
+		typedef myIter<const_pointer>						const_iterator;
+		typedef reverse_iterator<const_iterator>			const_reverse_iterator;
+		typedef reverse_iterator<iterator>					reverse_iterator;
     };
     
  
