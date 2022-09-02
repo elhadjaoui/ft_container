6281db14545dd9850faf9cfc132042d7fa242f2c
elhadjaoui <acharf-2060@outlook.fr>
2022-09-02 11:40:00 +0100

    pulling

--- header/vector.hpp
+++ header/vector.hpp
@@ -20,35 +20,27 @@
 #include <vector>
 #include <memory>
 
-
 namespace ft
 {
-    template < class T, class Alloc = std::allocator<T>> 
-    class vector
-    {
-    private:
-        
-    public:
-        public:
-		typedef T 											value_type;
-		typedef Alloc										allocator_type;
-		typedef typename allocator_type::reference			reference;
-		typedef typename allocator_type::const_reference 	const_reference;
-		typedef typename allocator_type::pointer			pointer;
-		typedef typename allocator_type::const_pointer		const_pointer;
-        typedef ptrdiff_t                                   difference_type;
-        typedef size_t										size_type;
-		typedef myIter<pointer>								iterator;
-		typedef myIter<const_pointer>						const_iterator;
-		typedef reverse_iterator<const_iterator>			const_reverse_iterator;
-		typedef reverse_iterator<iterator>					reverse_iterator;
-    };
-    
- 
-        
+	template <class T, class Alloc = std::allocator<T>>
+	class vector
+	{
+	private:
+	public:
+		typedef T value_type;
+		typedef Alloc allocator_type;
+		typedef typename allocator_type::reference reference;
+		typedef typename allocator_type::const_reference const_reference;
+		typedef typename allocator_type::pointer pointer;
+		typedef typename allocator_type::const_pointer const_pointer;
+		typedef ptrdiff_t difference_type;
+		typedef size_t size_type;
+		typedef myIter<pointer> iterator;
+		typedef myIter<const_pointer> const_iterator;
+		typedef reverse_iterator<const_iterator> const_reverse_iterator;
+		typedef reverse_iterator<iterator> reverse_iterator;
+	};
+
 }
 
-
-
-
-#endif 
+#endif
